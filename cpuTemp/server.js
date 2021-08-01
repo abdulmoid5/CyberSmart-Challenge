7531e24a362f110b088a337039c5151718f6a58e
Abdul Moid Babar <abdulmoidbabar@Abduls-MacBook-Pro.local>
2021-08-01 20:15:58 +0100

    tempApp initial commit

--- cpuTemp/server.js
+++ cpuTemp/server.js
@@ -1,15 +1,20 @@
 var express = require("express");
 var app = express();
 
+//connst defined for predefined sysinfo package
 const si = require("systeminformation");
 
+//method for OS specific platform & calling data
 app.get("/cputemp", function(req, res) {
   si.cpuTemperature().then(data => {
     res.status(200).send(data);
   });
 });
+
+//port set to local
 var port = process.env.PORT || 3000;
 
+//Launching to port
 var server = app.listen(port, function() {
-  console.log("Fetching CPU Information andƒ listening on port " + port);
+  console.log("Fetching CPU Information and listening on port " + port);
 });
